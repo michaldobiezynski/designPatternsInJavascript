b9de09351813d44b12c2ef9e773cfe7020c5ab11
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-10-05 08:47:24 +0100

    copy throught serialization

--- prototype/explicityCopying.js
+++ prototype/explicityCopying.js
@@ -1,56 +1,91 @@
-class Address
-{
+class Address {
   constructor(streetAddress, city, country) {
     this.streetAddress = streetAddress;
     this.city = city;
     this.country = country;
   }
 
-  deepCopy()
-  {
-    return new Address(
-      this.streetAddress,
-      this.city,
-      this.country
-    );
-  }
-
-  toString()
-  {
-    return `Address: ${this.streetAddress}, ` +
-      `${this.city}, ${this.country}`;
+  toString() {
+    return `Address: ${this.streetAddress}, ` + `${this.city}, ${this.country}`;
   }
 }
 
-class Person
-{
-  constructor(name, address)
-  {
+class Person {
+  constructor(name, address) {
     this.name = name;
     this.address = address; //
   }
 
-  deepCopy()
-  {
-    return new Person(
-      this.name,
-      this.address.deepCopy() // needs to be recursive
-    );
+  toString() {
+    return `${this.name} lives at ${this.address}`;
   }
 
-  toString()
-  {
-    return `${this.name} lives at ${this.address}`;
+  greet() {
+    console.log(
+      `Hi, my name is ${this.name}, ` + `I live at ${this.address.toString()}`
+    );
   }
 }
 
-let john = new Person('John',
-  new Address('123 London Road', 'London', 'UK'));
+class Serializer {
+  constructor(types) {
+    this.types = types;
+  }
 
-let jane = john.deepCopy();
+  markRecursive(object) {
+    // anoint each object with a type index
+    let idx = this.types.findIndex((t) => {
+      return t.name === object.constructor.name;
+    });
+    if (idx !== -1) {
+      object["typeIndex"] = idx;
 
-jane.name = 'Jane';
-jane.address.streetAddress = '321 Angel St'; // oops
+      for (let key in object) {
+        if (object.hasOwnProperty(key) && object[key] != null)
+          this.markRecursive(object[key]);
+      }
+    }
+  }
 
-console.log(john.toString()); // oops, john is called 'jane'
+  reconstructRecursive(object) {
+    if (object.hasOwnProperty("typeIndex")) {
+      let type = this.types[object.typeIndex];
+      let obj = new type();
+      for (let key in object) {
+        if (object.hasOwnProperty(key) && object[key] != null) {
+          obj[key] = this.reconstructRecursive(object[key]);
+        }
+      }
+      delete obj.typeIndex;
+      return obj;
+    }
+    return object;
+  }
+
+  clone(object) {
+    this.markRecursive(object);
+    let copy = JSON.parse(JSON.stringify(object));
+    return this.reconstructRecursive(copy);
+  }
+}
+
+let john = new Person("John", new Address("123 London Road", "London", "UK"));
+
+let jane = JSON.parse(JSON.stringify(john));
+
+jane.name = "Jane";
+jane.address.streetAddress = "321 Angel St";
+
+john.greet();
+// this won't work
+// jane.greet();
+
+// try a dedicated serializer
+let s = new Serializer([Person, Address]); // pain point
+jane = s.clone(john);
+
+jane.name = "Jane";
+jane.address.streetAddress = "321 Angel St";
+
+console.log(john.toString());
 console.log(jane.toString());
