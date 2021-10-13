db4bb4c322272f2628838fe5bf0ae5b524c91dff
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-10-13 20:55:33 +0100

    undo and redo

--- memento/memento.js
+++ memento/memento.js
@@ -7,31 +7,57 @@ class Memento {
 class BankAccount {
   constructor(balance = 0) {
     this.balance = balance;
+    this.changes = [new Memento(balance)];
+    this.current = 0;
   }
 
   deposit(amount) {
     this.balance += amount;
-    return new Memento(this.balance);
+    let m = new Memento(this.balance);
+    this.changes.push(m);
+    this.current++;
+    return m;
   }
 
   restore(m) {
-    this.balance = m.balance;
+    if (m) {
+      this.balance = m.balance;
+      this.changes.push(m);
+      this.current = this.changes.count - 1;
+    }
+  }
+
+  undo() {
+    if (this.current > 0) {
+      let m = this.changes[--this.current];
+      this.balance = m.balance;
+      return m;
+    }
+    return null;
+  }
+
+  redo() {
+    if (this.current + 1 < this.changes.length) {
+      let m = this.changes[++this.current];
+      this.balance = m.balance;
+      return m;
+    }
+    return null;
   }
 
   toString() {
-    return `Balance: ${this.balance}`;
+    return `Balance: $${this.balance}`;
   }
 }
 
 let ba = new BankAccount(100);
-let m1 = ba.deposit(50);
-let m2 = ba.deposit(25);
+ba.deposit(50);
+ba.deposit(25);
 console.log(ba.toString());
 
-// restore to m1
-ba.restore(m1);
-console.log(ba.toString());
-
-// restore to m2
-ba.restore(m2);
-console.log(ba.toString());
+ba.undo();
+console.log(`Undo 1: ${ba.toString()}`);
+ba.undo();
+console.log(`Undo 2: ${ba.toString()}`);
+ba.redo();
+console.log(`Redo 2: ${ba.toString()}`);
